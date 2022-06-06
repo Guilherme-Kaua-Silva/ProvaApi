35e36a4f9a366798bc878bb01f160c6a97164d85
Guilherme <pepe.ks.rx.1@gmail.com>
2022-06-06 18:07:54 -0300

    paraaaa

--- api/index.js
+++ api/index.js
@@ -4,13 +4,9 @@ import express from "express";
 import cors from "cors";
 import 'mysql2/promise'
 
-const serve = express();
+const server = express();
 server.use(cors());
 server.use(express.json());
-
-
-
-
 
 server.listen(process.env.PORT,() =>
                             console.log(`Porta de api em ${process.env.PORT}`));

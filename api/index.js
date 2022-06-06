9d1faaccf88be4cbac45fd07a735fc779b273474
Guilherme Kauã Silva <pepe.ks.rx.1@gmail.com>
2022-06-06 16:56:35 -0300

    okok

---
+++ api/index.js
@@ -0,0 +1,16 @@
+import "dotenv/config"
+
+import express from "express";
+import cors from "cors";
+import 'mysql2/promise'
+
+const serve = express();
+server.use(cors());
+server.use(express.json());
+
+
+
+
+
+server.listen(process.env.PORT,() =>
+                            console.log(`Porta de api em ${process.env.PORT}`));

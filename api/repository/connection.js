d889109963a70d5f3779ca58e9a5aabef58c546e
Guilherme <pepe.ks.rx.1@gmail.com>
2022-06-06 17:28:28 -0300

    initial commit

--- api/repository/connection.js
+++ api/repository/connection.js
@@ -1,2 +1,15 @@
 import 'mysql2/promise'
+import {con} from 'con'
 
+
+
+export function inserir(anime){
+    const comando = `
+    
+    
+    
+    
+    `;
+    const [linha] = await con.query(comando[anime]);
+    return linha; 
+}

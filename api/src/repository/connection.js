414d49908dcff56a39918299165743cd374745a3
Guilherme Kauã Silva <pepe.ks.rx.1@gmail.com>
2022-06-07 15:59:32 -0300

    okok

--- api/src/repository/connection.js
+++ api/src/repository/connection.js
@@ -4,8 +4,8 @@ import mysql from 'mysql2/promise'
 const con = await mysql.createConnection({
     host:process.env.MYSQL_HOST,
     user:process.env.MYSQL_USER,
-    pwd:process.env.MYSQL_PWD,
-    db:process.env.MYSQL_BD
+    password:process.env.MYSQL_PWD,
+    database:process.env.MYSQL_BD
 
 })
 

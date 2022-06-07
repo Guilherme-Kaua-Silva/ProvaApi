f101d35fc2ab17b87af5f2e8d68556f560535797
Guilherme Kauã Silva <pepe.ks.rx.1@gmail.com>
2022-06-07 16:10:55 -0300

    okok

--- api/src/repository/repository.js
+++ api/src/repository/repository.js
@@ -1,6 +1,7 @@
 import {con } from './connection.js'
 
 
+
 export async function inserir(anime){
     const comando = `
     insert into tb_animes(nm_anime)
@@ -9,3 +10,10 @@ export async function inserir(anime){
     const [linha] = await con.query(comando[anime.nome]);
     return linha.affectedRows; 
 }
+
+export async function selecionar(anime){
+    const comando = `
+    
+    `
+}
+
--- banco de dados/dml.sql
+++ banco de dados/dml.sql
@@ -1,5 +1,6 @@
 use aiai;
+
+select * from tb_animes;
+
 insert into tb_animes(nm_anime)
 	values ("dragon ball");
-    
-    
